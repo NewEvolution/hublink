76630d21301b000180e10f65d448999b98476926
Ryan Tanay <rtanay@gmail.com>
2025-01-16 18:39:36 -0600

    Pulling out NAT

--- vpc.tf
+++ vpc.tf
@@ -41,20 +41,8 @@ resource "aws_route_table" "public_route_table" {
   }
 }
 
-resource "aws_route_table" "private_route_table_A" {
+resource "aws_route_table" "private_route_table" {
   vpc_id = aws_vpc.hublink_vpc.id
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.elavon_A.id
-  }
-}
-
-resource "aws_route_table" "private_route_table_B" {
-  vpc_id = aws_vpc.hublink_vpc.id
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.elavon_B.id
-  }
 }
 
 resource "aws_route_table_association" "public_route_A" {
@@ -69,12 +57,12 @@ resource "aws_route_table_association" "public_route_B" {
 
 resource "aws_route_table_association" "private_route_A" {
   subnet_id      = aws_subnet.private_subnet_A.id
-  route_table_id = aws_route_table.private_route_table_A.id
+  route_table_id = aws_route_table.private_route_table.id
 }
 
 resource "aws_route_table_association" "private_route_B" {
   subnet_id      = aws_subnet.private_subnet_B.id
-  route_table_id = aws_route_table.private_route_table_B.id
+  route_table_id = aws_route_table.private_route_table.id
 }
 
 resource "aws_vpc_endpoint" "s3" {
@@ -84,8 +72,7 @@ resource "aws_vpc_endpoint" "s3" {
 
   route_table_ids = [
     aws_route_table.public_route_table.id,
-    aws_route_table.private_route_table_A.id,
-    aws_route_table.private_route_table_B.id
+    aws_route_table.private_route_table.id,
   ]
 
   tags = {
