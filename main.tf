e48e026f59147422a2957d9538d315ecc1ce9879
Ryan Tanay <rtanay@gmail.com>
2025-01-19 23:56:39 -0600

    Reorder in increasing specificity

--- main.tf
+++ main.tf
@@ -10,8 +10,8 @@ provider "aws" {
 terraform {
   required_version = "> 0.15.0"
   backend "s3" {
+    region = "CHANGEME"
     bucket = "CHANGEME"
     key    = "hublink.tfstate"
-    region = "CHANGEME"
   }
 }
